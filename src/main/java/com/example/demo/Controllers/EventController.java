099ff7356f3cba15aa54a2b84b78ce338d8ce670
rajalingamseth <64244526+rajalingamseth@users.noreply.github.com>
2025-10-06 22:35:16 +0530

    Added DTO class and few more validations on Hibernate field
    * Update README.md
    
    * Update README.md
    
    * Added DTO class and few more validations on Model class
    
    * Added DTO class and few more validations on Model class
    
    * Moved Stream logic to Service class and added delete mapping
    
    * Added custom exception handling into EventController
    
    * Updated the custom exception handler method

--- src/main/java/com/example/demo/Controllers/EventController.java
+++ src/main/java/com/example/demo/Controllers/EventController.java
@@ -1,9 +1,14 @@
 package com.example.demo.Controllers;
 
+import com.example.demo.ErrorHandling.CustomException;
+import com.example.demo.Models.EventDTO;
 import com.example.demo.Models.Events;
 import com.example.demo.Services.EventService;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -16,13 +21,28 @@ public class EventController {
     private EventService eventService;
 
     @GetMapping("Events")
-    public List<Events> getEventData(){
-        return null;
+    public ResponseEntity<List<EventDTO>> getEventData(){
+        return new ResponseEntity<>(eventService.getEventData(), HttpStatus.OK);
+    };
+
+    @PostMapping("addNewEvent")
+    public ResponseEntity<String> addNewEvent(@Valid @RequestBody Events event){
+        eventService.addEvent(event);
+        return new ResponseEntity<>("Successfully added new event", HttpStatus.OK);
     }
 
-    @PostMapping("addEvent")
-    public String  addEvent(@Valid @RequestBody Events event){
-        eventService.addEvent(event);
-        return "Success";
+    @DeleteMapping("deleteEvent/{id}")
+    public ResponseEntity<String> deleteEvent(@PathVariable String id) throws CustomException {
+        if (id == null || id.isBlank() || !id.matches("\\d+")) {
+            throw new CustomException("Invalid or missing event ID.");
+        }
+        int eventId = Integer.parseInt(id);
+        if(eventService.deleteEventById(eventId)){
+            return new ResponseEntity<>("Successfully removed event", HttpStatus.OK);
+        }
+        else {
+            return new ResponseEntity<>("Could not delete the event", HttpStatus.NOT_FOUND);
+        }
     }
+
 }
---
+++ src/main/java/com/example/demo/ErrorHandling/CustomException.java
@@ -0,0 +1,8 @@
+package com.example.demo.ErrorHandling;
+
+public class CustomException extends Exception {
+
+    public CustomException(String string){
+        super(string);
+    }
+}
